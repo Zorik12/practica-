5f2152e556dd464da8f7002177a2868d70435176
Zorik12 <arutunanzorik9@gmail.com>
2026-02-11 17:08:03 +0300

    Create run_full_init.sql

---
+++ sql/run_full_init.sql
@@ -0,0 +1,6 @@
+-- Файл: sql/run_full_init.sql
+-- Полная инициализация базы данных: удаление, создание, наполнение
+
+SOURCE sql/00_drop_tables.sql;
+SOURCE sql/01_create_tables.sql;
+SOURCE sql/02_insert_test_data.sql;
